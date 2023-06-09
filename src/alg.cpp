140bfda3099bce35a4e4229c66f516fe7c01fbfc
sharovsergey <125075282+sharovsergey@users.noreply.github.com>
2023-06-09 19:53:46 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,41 +1,68 @@
 // Copyright 2021 NNTU-CS
-
-int countPairs1(int *arr, int len, int value) {
-  return 0;
-  for(int i=0; i<n; i++)
-  {
-    for(int j=i+1; j<n; j++)
-    {
-      if(arr[i]+arr[j]==sum)
-        cout<<arr[i]<<" "<<arr[j]<<endl;
+void findPairs(vector<int>& arr, int sum)
+{
+    int n = arr.size();
+    unordered_map<int, int> m;
+    for (int i = 0; i < n; i++) {
+        int x = sum - arr[i];
+        if (m[x]) {
+            cout << arr[i] << " " << x << endl;
+        }
+        m[arr[i]]++;
     }
-  }
-
-int countPairs2(int *arr, int len, int value) {
-  return 0;
-  sort(arr, arr+n);
-  int i=0, j=n-1;
-  while(i<j)
-  {
-    if(arr[i]+arr[j]==sum)
-    {
-      cout<<arr[i]<<" "<<arr[j]<<endl;
-      i++; j--;
-    }
-    else if(arr[i]+arr[j]<sum)
-      i++;
-    else
-      j--;
-  }
 }
-int countPairs3(int *arr, int len, int value) {
-  return 0;
-  sort(arr, arr+n);
-  for(int i=0; i<n; i++)
-  {
-    int target = sum - arr[i];
-    int res = binary_search(arr+i+1, arr+n, target);
-    if(res)
-      cout<<arr[i]<<" "<<target<<endl;
-  }
+
+int main()
+{
+    vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int sum = 10;
+    findPairs(arr, sum);
+    return 0;
+}
+void findPairs(vector<int>& arr, int sum)
+{
+    int n = arr.size();
+    unordered_set<int> s;
+    for (int i = 0; i < n; i++) {
+        int x = sum - arr[i];
+        if (s.find(x) != s.end()) {
+            cout << arr[i] << " " << x << endl;
+        }
+        s.insert(arr[i]);
+    }
+}
+
+int main()
+{
+    vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int sum = 10;
+    findPairs(arr, sum);
+    return 0;
+}
+void findPairs(vector<int>& arr, int sum)
+{
+    int n = arr.size();
+    sort(arr.begin(), arr.end());
+    int i = 0, j = n - 1;
+    while (i < j) {
+        if (arr[i] + arr[j] == sum) {
+            cout << arr[i] << " " << arr[j] << endl;
+            i++;
+            j--;
+        }
+        else if (arr[i] + arr[j] < sum) {
+            i++;
+        }
+        else {
+            j--;
+        }
+    }
+}
+
+int main()
+{
+    vector<int> arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int sum = 10;
+    findPairs(arr, sum);
+    return 0;
 }
